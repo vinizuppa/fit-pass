c182913028eaf18665e52239fbeb625a1f7162ba
Vinicius Zuppa <vcordeiro12@gmail.com>
2021-10-19 21:41:26 -0300

    Usando cordova para tirar foto com a camera

--- src/pages/profile-aluno/profile-aluno.module.ts
+++ src/pages/profile-aluno/profile-aluno.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ProfileAlunoPage } from './profile-aluno';
+import { Camera } from '@ionic-native/camera';
 
 @NgModule({
   declarations: [
@@ -9,5 +10,8 @@ import { ProfileAlunoPage } from './profile-aluno';
   imports: [
     IonicPageModule.forChild(ProfileAlunoPage),
   ],
+  providers: [
+    Camera
+  ]
 })
 export class ProfileAlunoPageModule {}
--- src/pages/profile-aluno/profile-aluno.ts
+++ src/pages/profile-aluno/profile-aluno.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PARAMETERS } from '@angular/core/src/util/decorators';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { API_CONFIG } from '../../config/api.config';
 import { AlunoDTO } from '../../models/aluno.dto';
@@ -30,13 +31,17 @@ export class ProfileAlunoPage {
   localPerfil: PerfilDTO;
   alunoA: boolean;
   instrutorA: boolean;
+  picture: string;
+  cameraOn: boolean = false;
+
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
     public storage: StorageService,
     public alunoService: AlunoService,
     public instrutorService: InstrutorService,
-    public usuarioService: UsuarioService) {
+    public usuarioService: UsuarioService,
+    public camera: Camera) {
   }
 
   ionViewDidLoad() {
@@ -108,5 +113,24 @@ export class ProfileAlunoPage {
     }   
   } 
   
+  getCameraPicture(){
+    this.cameraOn = true;
+
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.PNG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64 (DATA_URL):
+     this.picture = 'data:image/png;base64,' + imageData;
+     this.cameraOn = false;
+    }, (err) => {
+     // Handle error
+    });
+  }
 
 }
